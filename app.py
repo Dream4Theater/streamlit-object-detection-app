f0c8236f580067fc885c4b26fb50b06f0b14dc03
Dream4Theater <81992744+Dream4Theater@users.noreply.github.com>
2021-05-20 03:12:54 +0300

    project upload

---
+++ app.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+import streamlit as st
+
+st.title("Object Detection with Yolov4-tiny")
+
+image_placeholder = st.empty()
+
+# Load Yolo
+net = cv2.dnn.readNet("yolov4-tiny.weights", "yolov4-tiny.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+cap = cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4,720)
+start = st.button("start")
+stop = st.button("stop")
+
+
+if start:
+    while True:
+        # Loading image
+        success, img = cap.read()
+        height, width, channels = img.shape
+        
+        # Detecting objects
+        blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+        net.setInput(blob)
+        outs = net.forward(output_layers)
+        
+        # Showing informations on the screen
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    # Object detected
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+                    # Rectangle coordinates
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+                    
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        
+        
+            
+        font = cv2.FONT_HERSHEY_PLAIN
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                color = colors[i]
+                cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+                cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
+        image_placeholder.image(img,channels="BGR")            
+        if stop:
+            cv2.destroyAllWindows()
+            break
