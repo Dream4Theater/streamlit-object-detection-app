66bc58961281862eba3899195fbda1842aef8890
Dream4Theater <81992744+Dream4Theater@users.noreply.github.com>
2021-06-06 13:01:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import cv2 
 import numpy as np
 import streamlit as st
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer, ClientSettings, AudioProcessorBase
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer, ClientSettings, AudioProcessorBase, WebRtcMode
 import argparse
 import av
 import pydub
