61f604f32f9d4d7bc05443225210504144fa113d
Dream4Theater <81992744+Dream4Theater@users.noreply.github.com>
2021-06-06 14:19:07 +0300

    update

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ import argparse
 import av
 import pydub
 import asyncio
-import speech_recognition as sr
-import pyttsx3
 
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
     rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
@@ -171,46 +169,3 @@ elif 2:
     # Python program to translate
     # speech to text and text to speech
     webrtc_ctx = webrtc_streamer(key="audio filter", client_settings=WEBRTC_CLIENT_SETTINGS, async_transform=True)
-    # Initialize the recognizer 
-    r = sr.Recognizer()
-    
-    # Function to convert text to
-    # speech
-    def SpeakText(command): 
-        # Initialize the engine
-        engine = pyttsx3.init()
-        engine.say(command)
-        engine.runAndWait()
-        
-    # Loop infinitely for user to
-    # speak
-    
-    while(1):    
-        
-        # Exception handling to handle
-        # exceptions at the runtime
-        try:
-            
-            # use the microphone as source for input.
-            with sr.Microphone() as source2:
-                
-                # wait for a second to let the recognizer
-                # adjust the energy threshold based on
-                # the surrounding noise level 
-                r.adjust_for_ambient_noise(source2, duration=0.2)
-                
-                #listens for the user's input 
-                audio2 = r.listen(source2)
-                
-                # Using ggogle to recognize audio
-                MyText = r.recognize_google(audio2)
-                MyText = MyText.lower()
-    
-                print("Did you say "+MyText)
-                SpeakText(MyText)
-                
-        except sr.RequestError as e:
-            print("Could not request results; {0}".format(e))
-            
-        except sr.UnknownValueError:
-            print("unknown error occured")
